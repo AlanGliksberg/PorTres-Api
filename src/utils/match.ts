782a32d10e2f91f0224960f81a97d35fd12be9a1
Alan Gliksberg <alan.glik@gmail.com>
2025-04-24 00:37:54 -0300

    ajuste creacion de player

--- src/utils/match.ts
+++ src/utils/match.ts
@@ -1,34 +1,10 @@
 import prisma from "../prisma/client";
 import { PlayerDTO } from "../types/playerTypes";
+import { createOrGetPlayers } from "./player";
 
 export const createTeam = async (teamNumber: number, players: PlayerDTO[]) => {
     return {
         teamNumber,
         players: { create: await createOrGetPlayers(players) }
     }
-}
-
-const createOrGetPlayers = async (players: PlayerDTO[]) => {
-    if (!players || players.length === 0) {
-        return [];
-    }
-
-    const playerPromises = players.map(async (player: PlayerDTO) => {
-        // if (player.id) {
-        //     return { playerId: player.id };
-        // } else {
-        const createdPlayer = await prisma.player.create({
-            data: {
-                name: player.name,
-                lastName: player.lastName,
-                level: player.level,
-                phone: player.phone
-            }
-        });
-        // return { playerId: createdPlayer.id };
-        return createdPlayer;
-        // }
-    });
-
-    return Promise.all(playerPromises);
 }
--- src/utils/player.ts
+++ src/utils/player.ts
@@ -1,4 +1,5 @@
 import prisma from "../prisma/client";
+import { PlayerDTO } from "../types/playerTypes";
 
 export const getPlayerByUserId = async (userId: string) => {
     return await prisma.player.findUnique({
@@ -6,4 +7,36 @@ export const getPlayerByUserId = async (userId: string) => {
             userId: userId,
         },
     });
+}
+
+export const getPlayerById = async (playerId: string) => {
+    return await prisma.player.findUnique({
+        where: {
+            id: playerId,
+        },
+    });
+}
+
+export const createOrGetPlayers = async (players: PlayerDTO[]) => {
+    if (!players || players.length === 0) {
+        return [];
+    }
+
+    const playerPromises = players.map(async (player: PlayerDTO) => {
+        if (player.id) {
+            return (await getPlayerById(player.id))!;
+        } else {
+            const createdPlayer = await prisma.player.create({
+                data: {
+                    name: player.name,
+                    lastName: player.lastName,
+                    level: player.level,
+                    phone: player.phone
+                }
+            });
+            return createdPlayer;
+        }
+    });
+
+    return Promise.all(playerPromises);
 }
