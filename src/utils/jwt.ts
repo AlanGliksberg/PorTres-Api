6d1e97caa48ec3cf8c38e00ac109361f8f7ce3d9
AlanGliksberg <alan.gliksberg@globons.com>
2025-05-17 13:13:09 -0300

    ajuste secret

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,4 +1,3 @@
 import jwt from "jsonwebtoken";
-const JWT_SECRET = process.env.JWT_SECRET!;
-export const signToken = (payload: object) => jwt.sign(payload, JWT_SECRET);
-export const verifyToken = (token: string) => jwt.verify(token, JWT_SECRET);
+export const signToken = (payload: object) => jwt.sign(payload, process.env.JWT_SECRET!);
+export const verifyToken = (token: string) => jwt.verify(token, process.env.JWT_SECRET!);
