c9ae0796988aa15c2488737f5ceb49c69d84467e
Alan Gliksberg <alan.glik@gmail.com>
2025-05-25 14:20:04 -0300

    ajusto db

--- prisma/migrations/20250517163607_agrego_duracion/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `duration` to the `Match` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Match" ADD COLUMN     "duration" INTEGER NOT NULL;
---
+++ prisma/migrations/20250525171906_duration/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Match" ADD COLUMN     "duration" INTEGER NOT NULL DEFAULT 90;
